161ed256a30fdb623aa9151fbe063504f031d38c
Shreya-2415 <127885027+Shreya-2415@users.noreply.github.com>
2023-07-28 15:42:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// This code will add a click event listener to the "Finding a job" link
-// When the link is clicked, it will open the "finding-a-job.html" page
-
-document.querySelector(".finding-a-job").addEventListener("click", function() {
-    window.location.href = "finding-a-job.html";
-  });
+function search() {
+    var keyword = document.querySelector("#search input").value;
+    var url = "https://indeed.com/jobs?q=" + keyword;
+    window.location.href = url;
+  }
   
+  document.querySelector("#search button").addEventListener("click", search);
