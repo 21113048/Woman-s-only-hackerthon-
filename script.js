df9f8f8008161599bb63e85028766ae0d2db2dfd
Shreya-2415 <127885027+Shreya-2415@users.noreply.github.com>
2023-08-05 01:04:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function search() {
-    var keyword = document.querySelector("#search input").value;
-    var url = "https://indeed.com/jobs?q=" + keyword;
-    window.location.href = url;
-  }
-  
-  document.querySelector("#search button").addEventListener("click", search);
